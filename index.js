046d7af6cf3401babcc8ed8244381ac1d039a7c8
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-02-06 18:41:38 -0500

    commit tree f1c0c1d4dc5af2cf505c4615b9028c6db4324ec9

--- index.js
+++
@@ -1,8 +0,0 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
-    function clickAlert() {
-        alert('I was clicked!');
-        }
-    input.addEventListener('click', clickAlert);
-}
-addingEventListener();
